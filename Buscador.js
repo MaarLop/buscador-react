abe2c6898c5a54c78344cd8d741190b0875ef0a1
MaarLop <s.mariel.lopez1995@gmail.com>
2019-01-12 15:17:41 -0300

    Se implemento test basicos para setear el filtro de categorias dependiendo si esta o no seteado el filtro de paises

--- Buscador.js
+++ Buscador.js
@@ -2,18 +2,28 @@ const mercadolibremod= require('./MercadoLibreClient')
 
 class Buscador{
     constructor (){
-       this.resultados= []
-       this.filtro= ''
+       this.categorias= []
+       this.filtroPaises= ''
+       this.filtroCategoria= ''
     }
     setearFiltro(_filtro){
-        this.filtro= _filtro
+        this.filtroPaises= _filtro
     }
 
     obtenerResultados(){
         let client = new mercadolibremod();
-        return client.getCategoriasPara(this.filtro).then((_responseCategoria) => {
-          this.resultados= _responseCategoria
+        return client.getCategoriasPara(this.filtroPaises).then((_responseCategoria) => {
+          this.categorias= _responseCategoria
         })
+    }
+
+    setearFiltroDeCategoria(_nombre_categoria){
+        if (this.filtroPaises !== ''){
+            this.filtroCategoria= _nombre_categoria
+        }
+        else{
+            throw new Error ('Ingrese Pais')
+        }
     }
 }
 
--- test/BuscadorTest.js
+++ test/BuscadorTest.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert
 const buscadormod= require('../Buscador')
+const chai= require('chai')
 
 describe('Resultados de busqueda', function (){
     let buscador= null
@@ -7,24 +8,44 @@ describe('Resultados de busqueda', function (){
     beforeEach(() => {
         buscador = new buscadormod();
       });
+   
+    //TEST DE INICIALIZACION
 
     it ( 'Retorna 0 porque la lista de resultados esta vacia', function (){
-        assert.equal( buscador.resultados.length, 0 )
+        assert.equal( buscador.categorias.length, 0 )
     })
 
     it ('Retorna un string vacio ya que al estar en estado inicial este no fue inicializado aun', function(){
-        assert.equal(buscador.filtro, '')
+        assert.equal(buscador.filtroPaises, '')
     })
+
+    it ('Retorna vacio el filtro de categorias ya que el de paises tambien lo esta', function(){
+        assert.equal(buscador.filtroCategoria, '')
+    })
+
+    //TEST DE FILTRO DE CATEGORIAS POR PAISES
 
     it ('Retorna MLA al realizar la consulta despues de inicializar el filtro en dicho parameto', function(){
         buscador.setearFiltro('MLA')
-        assert.equal(buscador.filtro, 'MLA')
+        assert.equal(buscador.filtroPaises, 'MLA')
     })
 
     it( 'Retorna que la lista de resultados ya no es vacia al setearle el parametro MLA al filtro del buscador', function(){
         buscador.setearFiltro('MLA')
-        return buscador.obtenerResultados().then ( function() {
-            assert.notEqual(buscador.resultados, 0)
+        return buscador.obtenerResultados().then ( function(r) {
+            assert.notEqual(buscador.categorias, 0)
         })
+    })
+
+ 
+    // TEST DE FILTRO DE ARTICULOS DEPENDIENDO CATEGORIA Y PAIS
+
+    it( 'Al setear un valor al campo de filtro de paises se puede setear el filtro de categorias', function(){
+        buscador.setearFiltro('MLA')
+        buscador.setearFiltroDeCategoria('MLA572')
+        assert.equal(buscador.filtroCategoria, 'MLA572')
+    })
+    it('Al no setearse un valor en el campo de filtro de paieses no se seteara el valor al campo de filtro de categorias', function(){
+        chai.expect(() => buscador.setearFiltroDeCategoria('MLA572')).to.throw('Ingrese Pais');
     })
 })
